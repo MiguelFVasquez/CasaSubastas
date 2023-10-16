7564a25e0769570e0dbe10d49ab2f1bb7e99604c
MiguelFVasquez <126723056+MiguelFVasquez@users.noreply.github.com>
2023-10-16 01:42:17 -0500

    Configuracion para eliminar el producto, tener en cuenta que falla algo en la creacion

---
+++ src/main/java/co/edu/uniquindio/pr3/subastas/controllers/AnuncianteController.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.pr3.subastas.controllers;
+
+public class AnuncianteController {
+
+    public ModelFactoryController mfm;
+
+    public AnuncianteController(){
+        mfm= ModelFactoryController.getInstance();
+    }
+
+}
